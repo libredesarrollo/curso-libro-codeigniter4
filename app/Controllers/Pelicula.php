52a9ed003e24588b2065979eae4ecad6d26993c4
libredesarrollo <desarrollolibre.net@gmail.com>
2022-01-17 15:01:24 -0400

    Proyecto curso

---
+++ app/Controllers/Pelicula.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Pelicula extends BaseController
+{
+    public function new()
+    {
+        echo "Hola";
+    }
+
+    public function index()
+    {
+        echo "<h1>Hola Mundo</h1>";
+    }
+
+    public function create()
+    {
+    }
+
+    public function edit($id = null)
+    {
+    }
+
+    public function update($id = null)
+    {
+    }
+
+    public function delete($id = null)
+    {
+    }
+}
