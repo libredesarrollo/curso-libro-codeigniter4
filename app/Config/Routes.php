9cb6188b5805bab796daae48b74695fffa4666c2
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2025-01-12 10:27:16 -0400

    c4 2025

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,63 +1,18 @@
 <?php
 
-namespace Config;
+use CodeIgniter\Router\RouteCollection;
 
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
-
-// Load the system's routing file first, so that the app and ENVIRONMENT
-// can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
-    require SYSTEMPATH . 'Config/Routes.php';
-}
-
-
-/*
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
+/**
+ * @var RouteCollection $routes
  */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(false);
-
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
- */
-
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
-// //crear
-$routes->get('pelicula/new', 'Pelicula::new', ['as' => 'pelicula.new']);
-// $routes->post('pelicula/create', 'Pelicula::create');
-
-// // ver
-// $routes->get('pelicula', 'Pelicula::index');
-// $routes->get('pelicula/(:num)', 'Pelicula::show/$1');
-
-// // actualizar
-// $routes->get('pelicula/(:num)/edit', 'Pelicula::edit/$1');
-// $routes->post('pelicula/update/(:num)', 'Pelicula::update/$1');
-// //$routes->put('pelicula/(:num)','Pelicula::update/$1');
-
-// // eliminar
-// $routes->post('pelicula/delete/(:num)', 'Pelicula::delete/$1');
-// $routes->delete('pelicula/(:num)', 'Pelicula::update/$1');
-
-//['namespace' => 'App\Controllers']
 
 $routes->group('dashboard', ['namespace' => '\App\Controllers\Dashboard'], function ($routes) {
     $routes->presenter('pelicula');
     $routes->presenter('categoria');
 }); //3UH25970119751840
 
+$routes->get('pelicula/new', 'Pelicula::new', ['as' => 'pelicula.new']);
 
 $routes->get('login', '\App\Controllers\Web\Usuario::login', ['as' => 'usuario.login']);
 $routes->post('login', '\App\Controllers\Web\Usuario::login_post', ['as' => 'usuario.login.post']);
@@ -75,22 +30,3 @@ $routes->group('paypal', function ($routes) {
     $routes->post('process/(:alphanum)', '\App\Controllers\PayPal\PaymentPaypal::process/$1', ['as' => 'paypal-process']);
     $routes->get('', '\App\Controllers\PayPal\PaymentPaypal::index');
 });
-
-
-//$routes->presenter('/dashboard/categoria', ['controller' =>'\App\Controllers\Categoria']);
-/*
- * --------------------------------------------------------------------
- * Additional Routing
- * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
- */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
-}
--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -2,24 +2,25 @@
 
 namespace Config;
 
-use CodeIgniter\Validation\CreditCardRules;
-use CodeIgniter\Validation\FileRules;
-use CodeIgniter\Validation\FormatRules;
-use CodeIgniter\Validation\Rules;
+use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Validation\StrictRules\CreditCardRules;
+use CodeIgniter\Validation\StrictRules\FileRules;
+use CodeIgniter\Validation\StrictRules\FormatRules;
+use CodeIgniter\Validation\StrictRules\Rules;
 
-class Validation
+class Validation extends BaseConfig
 {
-    //--------------------------------------------------------------------
+    // --------------------------------------------------------------------
     // Setup
-    //--------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
     /**
      * Stores the classes that contain the
      * rules that are available.
      *
-     * @var string[]
+     * @var list<string>
      */
-    public $ruleSets = [
+    public array $ruleSets = [
         Rules::class,
         FormatRules::class,
         FileRules::class,
@@ -32,27 +33,12 @@ class Validation
      *
      * @var array<string, string>
      */
-    public $templates = [
+    public array $templates = [
         'list'   => 'CodeIgniter\Validation\Views\list',
         'single' => 'CodeIgniter\Validation\Views\single',
     ];
 
-    //--------------------------------------------------------------------
+    // --------------------------------------------------------------------
     // Rules
-    //--------------------------------------------------------------------
-
-    public $categorias = [
-        'titulo' => 'required|min_length[3]|max_length[255]'
-    ];
-    public $peliculas = [
-        'titulo' => 'required|min_length[3]|max_length[255]',
-        'descripcion' => 'required|min_length[3]|max_length[2000]',
-        'categoria_id' => 'required|is_natural',
-    ];
-
-    public $usuarios = [
-        'usuario' => 'required|min_length[3]|max_length[20]|is_unique[usuarios.usuario]',
-        'email' => 'required|min_length[3]|max_length[20]|is_unique[usuarios.email]',
-        'contrasena' => 'required|min_length[5]|max_length[15]'
-    ];
+    // --------------------------------------------------------------------
 }
