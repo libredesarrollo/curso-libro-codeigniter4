d85cb357e31168cc13f3158413c23d5991a44b9d
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2025-01-17 05:39:03 -0400

    varios

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-
+// header('Access-Control-Allow-Origin: *');
+// header("Access-Control-Allow-Methods: GET, OPTIONS");
 /*
  *---------------------------------------------------------------
  * CHECK PHP VERSION
