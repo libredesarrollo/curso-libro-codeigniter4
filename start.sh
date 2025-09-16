f3ff4a87d6f5942e2785612fd7412b2355a17ff3
Andres Cruz <desarrollolibre.net@gmail.com>
2025-09-16 08:15:32 -0400

    prueba

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-# Reemplaza $PORT en nginx.conf
-envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default_run.conf
+#!/bin/bash
 
 # Arranca PHP-FPM
 php-fpm &
 
-# Arranca Nginx con la configuración reemplazada
-nginx -g "daemon off;" -c /etc/nginx/nginx.conf
+# Arranca Nginx en primer plano
+nginx -g "daemon off;"
